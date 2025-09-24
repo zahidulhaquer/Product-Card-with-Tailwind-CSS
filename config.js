cdc4a23420680c7c05815dd4bcc837a8b0967acf
Zahidul Haque <zahidulhaquer@gmail.com>
2025-09-24 20:45:57 +0600

    First commit 09-24-2025

---
+++ config.js
@@ -0,0 +1,11 @@
+tailwind.config = {
+    theme: {
+        extend: {
+            colors: {
+                pale_orange: '#ffd9a6',
+                light_orange: '#fbb03b',
+                orange: '#f7931e'
+            }
+        }
+    }
+}
